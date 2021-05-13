1647a1a2cee1babf8f17d26ccf8d33113409d842
piyush <pg3523@gmail.com>
2021-05-13 14:34:42 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "covidreport.cf1jrcs9ijpi.us-east-2.rds.amazonaws.com"
+customuser = "piyushgupta"
+custompass = "piyushgupta"
+customdb = "covidreport"
+custombucket = "addcovidreport"
+customregion = "us-east-2a"
